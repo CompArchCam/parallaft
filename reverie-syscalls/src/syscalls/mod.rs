17c2a80048304dc3c0e62c305d4d59ff93dad184
Peter Zhang <mbyzhang@outlook.com>
2023-05-24 13:11:39 +0000

    reverie-syscalls: implement SyscallMayRead/Write for unlink

--- reverie-syscalls/src/syscalls/mod.rs
+++ reverie-syscalls/src/syscalls/mod.rs
@@ -1429,7 +1429,10 @@ typed_syscall! {
 // Unlink not available in aarch64
 #[cfg(not(target_arch = "aarch64"))]
 typed_syscall! {
+    #[may_read_specified_only]
+    #[may_write_specified_only]
     pub struct Unlink {
+        #[path_ptr_may_read]
         path: Option<PathPtr>,
     }
 }
