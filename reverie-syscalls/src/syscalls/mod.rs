29391d0d12b8ecf53558df1c00ed99c1635a7aea
Peter Zhang <me@mby.pw>
2023-08-22 16:50:38 +0100

    reverie-syscalls: implement SyscallMayRead/Write for mkdir

--- reverie-syscalls/src/syscalls/mod.rs
+++ reverie-syscalls/src/syscalls/mod.rs
@@ -1404,7 +1404,10 @@ typed_syscall! {
 // Mkdir not available in aarch64
 #[cfg(not(target_arch = "aarch64"))]
 typed_syscall! {
+    #[may_read_specified_only]
+    #[may_write_specified_only]
     pub struct Mkdir {
+        #[path_ptr_may_read]
         path: Option<PathPtr>,
         mode: Mode,
     }
