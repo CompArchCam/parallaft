235f00e89b4a5314281ee2cdc92e2da8814ac8f7
Peter Zhang <me@mby.pw>
2023-11-02 00:56:36 +0000

    tests: fix unsupported syscall test as readv is now supported

--- reldrv/tests/syscalls/unsupported.rs
+++ reldrv/tests/syscalls/unsupported.rs
@@ -7,14 +7,14 @@ use serial_test::serial;
 #[test]
 #[serial]
 #[should_panic]
-fn readv() {
+fn preadv() {
     setup();
     assert_eq!(
         trace(|| {
             checkpoint_take();
             let fd = File::open("/dev/zero").unwrap();
             let mut buf = [0u8; 16];
-            uio::readv(fd, &mut [IoSliceMut::new(&mut buf)]).unwrap();
+            uio::preadv(fd, &mut [IoSliceMut::new(&mut buf)], 0).unwrap();
             checkpoint_fini();
             0
         }),
