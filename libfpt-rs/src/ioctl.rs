d2df0d06e9ec95870916f3b7461a1377de28c440
Peter Zhang <me@mby.pw>
2023-11-22 03:49:15 +0000

    libfpt-rs: support for more ioctls and zero-copy mmap

--- libfpt-rs/src/ioctl.rs
+++ libfpt-rs/src/ioctl.rs
@@ -1,5 +1,6 @@
 use nix::ioctl_none;
 
+use nix::ioctl_read;
 use nix::ioctl_write_ptr;
 use nix::libc;
 
@@ -23,3 +24,7 @@ ioctl_none!(fptioc_clear_fault, b'k', 1);
 ioctl_write_ptr!(fptioc_read_fault, b'k', 2, FptReadFaultRequest);
 ioctl_none!(fptioc_enable, b'k', 3);
 ioctl_none!(fptioc_disable, b'k', 4);
+ioctl_read!(fptioc_get_lost_count, b'k', 5, libc::size_t);
+ioctl_read!(fptioc_get_count, b'k', 6, libc::size_t);
+ioctl_none!(fptioc_new_buffer, b'k', 7);
+ioctl_read!(fptioc_get_buffer_size, b'k', 8, libc::size_t);
--- libfpt-rs/src/lib.rs
+++ libfpt-rs/src/lib.rs
@@ -4,10 +4,55 @@ use bitflags::bitflags;
 use ioctl::{FptAttachRequest, FptReadFaultRequest};
 use nix::fcntl::OFlag;
 use nix::libc;
+use nix::sys::mman::{MapFlags, ProtFlags};
 use nix::sys::stat::Mode;
 use nix::unistd::{self, Pid};
 use nix::{fcntl, Result};
-use std::os::fd::{AsRawFd, RawFd};
+use std::mem::MaybeUninit;
+use std::num::NonZeroUsize;
+use std::os::fd::{AsFd, AsRawFd, BorrowedFd, RawFd};
+
+pub struct FptRecord {
+    addr: *const usize,
+    data_length: usize,
+    map_length: usize,
+}
+
+impl FptRecord {
+    pub(crate) fn new(fd: &FptFd) -> Result<FptRecord> {
+        let sz = fd.get_buffer_size()? * std::mem::size_of::<usize>();
+        let data_len = fd.get_count()?;
+
+        let addr = unsafe {
+            nix::sys::mman::mmap(
+                None,
+                NonZeroUsize::new_unchecked(sz),
+                ProtFlags::PROT_READ,
+                MapFlags::MAP_PRIVATE,
+                Some(fd),
+                0,
+            )?
+        };
+
+        Ok(Self {
+            addr: addr as _,
+            data_length: data_len,
+            map_length: sz,
+        })
+    }
+}
+
+impl AsRef<[usize]> for FptRecord {
+    fn as_ref(&self) -> &[usize] {
+        unsafe { std::slice::from_raw_parts(self.addr, self.data_length) }
+    }
+}
+
+impl Drop for FptRecord {
+    fn drop(&mut self) {
+        unsafe { nix::sys::mman::munmap(self.addr as *mut _, self.map_length).ok() };
+    }
+}
 
 pub struct FptFd {
     fd: RawFd,
@@ -19,6 +64,7 @@ bitflags! {
         const SIGTRAP_FULL = 0b0010;
         const SIGTRAP_WATERMARK = 0b0100;
         const SIGTRAP_WATERMARK_USER = 0b1000;
+        const ALLOW_REALLOC = 0b10000;
     }
 }
 
@@ -63,7 +109,7 @@ impl FptFd {
         unsafe { ioctl::fptioc_clear_fault(self.fd).map(|_| ()) }
     }
 
-    pub fn read_fault(&mut self, buf: &mut [*const u8], offset: usize) -> Result<usize> {
+    pub fn read_fault(&self, buf: &mut [usize], offset: usize) -> Result<usize> {
         unsafe {
             ioctl::fptioc_read_fault(
                 self.fd,
@@ -75,6 +121,46 @@ impl FptFd {
             )
             .map(|x| x as _)
         }
+    }
+
+    pub fn get_count(&self) -> Result<usize> {
+        let mut count = MaybeUninit::<libc::size_t>::uninit();
+        unsafe {
+            ioctl::fptioc_get_count(self.fd, count.as_mut_ptr())?;
+            Ok(count.assume_init() as _)
+        }
+    }
+
+    pub fn get_lost_count(&self) -> Result<usize> {
+        let mut count = MaybeUninit::<libc::size_t>::uninit();
+        unsafe {
+            ioctl::fptioc_get_lost_count(self.fd, count.as_mut_ptr())?;
+            Ok(count.assume_init() as _)
+        }
+    }
+
+    pub fn get_buffer_size(&self) -> Result<usize> {
+        let mut sz = MaybeUninit::<libc::size_t>::uninit();
+        unsafe {
+            ioctl::fptioc_get_buffer_size(self.fd, sz.as_mut_ptr())?;
+            Ok(sz.assume_init() as _)
+        }
+    }
+
+    pub fn new_buffer(&self) -> Result<()> {
+        unsafe { ioctl::fptioc_new_buffer(self.fd).map(|_| ()) }
+    }
+
+    pub fn take_record(&self) -> Result<FptRecord> {
+        let record = FptRecord::new(self)?;
+        self.new_buffer()?;
+        Ok(record)
+    }
+}
+
+impl AsFd for FptFd {
+    fn as_fd(&self) -> BorrowedFd<'_> {
+        unsafe { BorrowedFd::borrow_raw(self.fd) }
     }
 }
 
@@ -92,6 +178,8 @@ impl Drop for FptFd {
 
 #[cfg(test)]
 mod tests {
+    use std::os::fd::OwnedFd;
+
     use nix::{
         sys::{
             signal::{
@@ -102,6 +190,12 @@ mod tests {
         },
         unistd::{fork, sysconf, SysconfVar},
     };
+
+    use lazy_static::lazy_static;
+
+    lazy_static! {
+        static ref PAGE_SIZE: usize = sysconf(SysconfVar::PAGE_SIZE).unwrap().unwrap() as usize;
+    }
 
     use super::*;
 
@@ -118,12 +212,14 @@ mod tests {
         }
     }
 
-    fn continue_subprocess(pid: Pid) {
+    fn wait_for_sigstop(pid: Pid) {
         assert_eq!(
             waitpid(pid, Some(WaitPidFlag::WSTOPPED)).unwrap(),
             WaitStatus::Stopped(pid, SIGSTOP)
         );
+    }
 
+    fn continue_subprocess(pid: Pid) {
         kill(pid, SIGCONT).unwrap();
     }
 
@@ -137,10 +233,35 @@ mod tests {
         }
     }
 
+    fn get_shared_pointer<T>() -> Result<*mut *const T> {
+        unsafe {
+            nix::sys::mman::mmap::<OwnedFd>(
+                None,
+                NonZeroUsize::new_unchecked(*PAGE_SIZE),
+                ProtFlags::PROT_READ | ProtFlags::PROT_WRITE,
+                MapFlags::MAP_ANONYMOUS | MapFlags::MAP_SHARED,
+                None,
+                0,
+            )
+            .map(|p| p as *mut *const T)
+        }
+    }
+
+    fn put_shared_pointer<T>(ptr: *mut *const T) -> Result<()> {
+        unsafe { nix::sys::mman::munmap(ptr as _, *PAGE_SIZE).map(|_| ()) }
+    }
+
+    fn get_num_pages_in_range(list: &[usize], addr: usize, size: usize) -> usize {
+        list.into_iter()
+            .filter(|&&a| (a >= addr & (!(*PAGE_SIZE - 1))) && (a <= (addr + size)))
+            .count()
+    }
+
     #[test]
     fn test_open_close_fpt() {
         let pid = subprocess(|| {});
         let fd = FptFd::new(pid, 1024, FptFlags::empty(), None).unwrap();
+        wait_for_sigstop(pid);
         continue_subprocess(pid);
         wait_for_finish(pid);
 
@@ -148,27 +269,157 @@ mod tests {
     }
 
     #[test]
-    fn test_full() {
-        let page_size = sysconf(SysconfVar::PAGE_SIZE).unwrap().unwrap() as usize;
+    fn test_read_fault() {
         let cnt = 1024;
 
+        let vec_addr_p = get_shared_pointer().unwrap();
+
         let pid = subprocess(|| {
-            let mut v = vec![0u8; page_size * cnt];
+            let mut v = vec![0u8; *PAGE_SIZE * cnt];
+            unsafe { *vec_addr_p = v.as_ptr() };
 
             v.fill(42);
         });
 
-        let mut fd = FptFd::new(pid, 1024, FptFlags::empty(), None).unwrap();
+        let mut fd = FptFd::new(pid, 2048, FptFlags::empty(), None).unwrap();
 
+        wait_for_sigstop(pid);
         fd.enable().unwrap();
         continue_subprocess(pid);
         wait_for_finish(pid);
         fd.disable().unwrap();
 
-        let mut buf = vec![0 as *const u8; 4096];
+        let mut buf = vec![0 as usize; 4096];
         let s = fd.read_fault(&mut buf, 0).unwrap();
 
-        assert!(s >= cnt)
+        assert!(s >= cnt);
+
+        let num_dirty_pages_in_vec =
+            get_num_pages_in_range(&buf, unsafe { *vec_addr_p } as _, cnt * *PAGE_SIZE);
+        assert!(num_dirty_pages_in_vec >= cnt);
+        put_shared_pointer(vec_addr_p).unwrap();
+    }
+
+    #[test]
+    fn test_take_record_single() {
+        let cnt = 1024;
+        let vec_addr_p = get_shared_pointer().unwrap();
+
+        let pid = subprocess(|| {
+            let mut v = vec![0u8; *PAGE_SIZE * cnt];
+            unsafe { *vec_addr_p = v.as_ptr() };
+
+            v.fill(42);
+        });
+
+        let mut fd = FptFd::new(pid, 2048, FptFlags::empty(), None).unwrap();
+
+        wait_for_sigstop(pid);
+        fd.enable().unwrap();
+        continue_subprocess(pid);
+        wait_for_finish(pid);
+        fd.disable().unwrap();
+
+        let record = fd.take_record().unwrap();
+
+        let buf = record.as_ref();
+        assert!(buf.len() >= cnt);
+
+        let num_dirty_pages_in_vec =
+            get_num_pages_in_range(&buf, unsafe { *vec_addr_p } as _, cnt * *PAGE_SIZE);
+        assert!(num_dirty_pages_in_vec >= cnt);
+        put_shared_pointer(vec_addr_p).unwrap();
+    }
+
+    #[test]
+    fn test_take_record_multiple() {
+        let cnt = 1024;
+        let vec1_addr_p = get_shared_pointer().unwrap();
+        let vec2_addr_p = get_shared_pointer().unwrap();
+
+        let pid = subprocess(|| {
+            let mut v1 = vec![0u8; *PAGE_SIZE * cnt];
+            unsafe { *vec1_addr_p = v1.as_ptr() };
+            v1.fill(42);
+
+            raise(SIGSTOP).unwrap();
+
+            let mut v2 = vec![0u8; *PAGE_SIZE * cnt];
+            unsafe { *vec2_addr_p = v2.as_ptr() };
+            v2.fill(42);
+        });
+
+        let mut fd = FptFd::new(pid, 2048, FptFlags::empty(), None).unwrap();
+
+        wait_for_sigstop(pid);
+        fd.enable().unwrap();
+        continue_subprocess(pid);
+        wait_for_sigstop(pid);
+        fd.disable().unwrap();
+        let record1 = fd.take_record().unwrap();
+
+        fd.enable().unwrap();
+        continue_subprocess(pid);
+        wait_for_finish(pid);
+        fd.disable().unwrap();
+        let record2 = fd.take_record().unwrap();
+
+        let buf1 = record1.as_ref();
+        // dbg!(unsafe { std::mem::transmute::<_, &[*const u8]>(buf1) });
+
+        assert!(buf1.len() >= cnt);
+        assert!(buf1.len() < cnt * 2);
+        let num_dirty_pages_in_vec1 =
+            get_num_pages_in_range(&buf1, unsafe { *vec1_addr_p } as _, cnt * *PAGE_SIZE);
+        assert!(num_dirty_pages_in_vec1 >= cnt);
+
+        let buf2 = record2.as_ref();
+        // dbg!(unsafe { std::mem::transmute::<_, &[*const u8]>(buf2) });
+        assert!(buf2.len() >= cnt);
+        assert!(buf2.len() < cnt * 2);
+        let num_dirty_pages_in_vec2 =
+            get_num_pages_in_range(&buf2, unsafe { *vec2_addr_p } as _, cnt * *PAGE_SIZE);
+        assert!(num_dirty_pages_in_vec2 >= cnt);
+
+        put_shared_pointer(vec1_addr_p).unwrap();
+        put_shared_pointer(vec2_addr_p).unwrap();
+    }
+
+    #[test]
+    fn test_realloc() {
+        let cnt = 4096;
+        let vec_addr_p = get_shared_pointer().unwrap();
+
+        let pid = subprocess(|| {
+            let mut v = vec![0u8; *PAGE_SIZE * cnt];
+            unsafe { *vec_addr_p = v.as_ptr() };
+
+            v.fill(42);
+        });
+
+        let mut fd = FptFd::new(pid, 512, FptFlags::ALLOW_REALLOC, None).unwrap();
+
+        wait_for_sigstop(pid);
+        fd.enable().unwrap();
+        continue_subprocess(pid);
+        wait_for_finish(pid);
+        fd.disable().unwrap();
+
+        let lost_count = fd.get_lost_count().unwrap();
+        assert_eq!(lost_count, 0);
+
+        let total_count = fd.get_count().unwrap();
+        assert!(total_count >= cnt);
+
+        let record = fd.take_record().unwrap();
+
+        let buf = record.as_ref();
+        assert!(buf.len() >= cnt);
+
+        let num_dirty_pages_in_vec =
+            get_num_pages_in_range(&buf, unsafe { *vec_addr_p } as _, cnt * *PAGE_SIZE);
+        assert!(num_dirty_pages_in_vec >= cnt);
+        put_shared_pointer(vec_addr_p).unwrap();
     }
 
     // TODO: test SIGTRAP
