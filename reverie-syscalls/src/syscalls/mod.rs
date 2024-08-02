baf183c72a267e314fefe0d38f34c78c11ec3e8d
Peter <me@mby.pw>
2024-08-02 13:09:49 +0100

    reverie-syscalls: implement SyscallMayRead/Write for fstatat

--- reverie-syscalls/src/syscalls/mod.rs
+++ reverie-syscalls/src/syscalls/mod.rs
@@ -2993,9 +2993,13 @@ pub type Fstatat = Newfstatat;
 
 #[cfg(target_arch = "aarch64")]
 typed_syscall! {
+    #[may_read_specified_only]
+    #[may_write_specified_only]
     pub struct Fstatat {
         dirfd: i32,
+        #[path_ptr_may_read]
         path: Option<PathPtr>,
+        #[object_may_written]
         stat: Option<StatPtr>,
         flags: AtFlags,
     }
